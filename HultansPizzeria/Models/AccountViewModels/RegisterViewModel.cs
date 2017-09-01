dcc01e5fbd1f8ada1b263fb5c1337d27a3b4ec50
Marcus Hultkrantz <marcus.hultkrantz@live.se>
2017-09-01 21:41:14 +0200

    Updated registration model and view to fit the new ApplicationUser model

--- HultansPizzeria/Models/AccountViewModels/RegisterViewModel.cs
+++ HultansPizzeria/Models/AccountViewModels/RegisterViewModel.cs
@@ -23,5 +23,31 @@ namespace HultansPizzeria.Models.AccountViewModels
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [Display(Name = "Förnamn")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [Display(Name = "Efternamn")]
+        public string LastName { get; set; }
+
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [Display(Name = "Adress")]
+        public string Address { get; set; }
+
+        [StringLength(10, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 1)]
+        [Display(Name = "Lägenhetsnummer")]
+        public string ApartmentNumber { get; set; }
+
+        [StringLength(3, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 1)]
+        [Display(Name = "Våning")]
+        public string Floor { get; set; }
+
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 1)]
+        [Display(Name = "Portkod")]
+        public int EntryCode { get; set; }
     }
 }
