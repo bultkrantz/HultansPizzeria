41823045bc288d3efec66f989d27fee0742e8bcd
Marcus Hultkrantz <marcus.hultkrantz@live.se>
2017-09-02 17:18:17 +0200

    AdminController now authorized with admin role

--- HultansPizzeria/Controllers/AdminController.cs
+++ HultansPizzeria/Controllers/AdminController.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using HultansPizzeria.Data;
+using Microsoft.AspNetCore.Authorization;
 
 namespace HultansPizzeria.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
         private ApplicationDbContext _context;
